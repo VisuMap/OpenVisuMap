f899a57e3b728c18ad16e7b920752da67fa0f8c0
James Li <jamesxli@visumap.com>
2020-06-17 10:23:28 -0600

    Initial check-in

---
+++ TsneDx/Package.bat
@@ -0,0 +1,7 @@
+﻿del TsneDx.zip
+mkdir TsneDx
+for %%f in (TsneDx.exe SharpDX.Direct3D11.dll SharpDX.dll SharpDX.DXGI.dll TsneDx.exe.config ) do copy bin\Release\%%f TsneDx
+copy Readme.md TsneDx
+copy SP500.csv TsneDx
+zip -r TsneDx.zip TsneDx
+rmdir/s /Q TsneDx
