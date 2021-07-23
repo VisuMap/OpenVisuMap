f600f2b12adc2947d892087c69a6ee1caf2d0d60
James Li <jamesxli@visumap.com>
2021-07-23 11:31:16 -0600

    Adjusted the type of scaling parameter.

--- ClipRecorder/Install.js
+++ ClipRecorder/Install.js
@@ -1,3 +1,3 @@
 var dir = vv.CurrentScriptPath.substr(0, vv.CurrentScriptPath.lastIndexOf("\\") + 1);
-vv.InstallGlyphSet("VectorField", dir + "VectorField", false);
+vv.InstallGlyphSet("VectorField", dir + "VectorField", 0, 1.0);
 vv.InstallPlugin("Clip Recorder", dir +"ClipRecorder.dll");
