052c88b9a3d9d1e3fbfaad827d9b1538bec4e39c
James Li <jamesxli@visumap.com>
2021-10-02 15:40:44 -0600

    Significant performance enhancemnts

--- GeneticAnalysis/ExtractSeq.js
+++ GeneticAnalysis/ExtractSeq.js
@@ -2,25 +2,73 @@ var ds = vv.Dataset;
 var blobName = ds.ColumnSpecList[0].Name;
 var sm = vv.FindPluginObject("SeqAnalysis").OpenSequence(blobName);
 
-var cs = New.CsObject("\
-	Random rg = new Random();\
-	public string RandomizeSeq(string s, double p) {\
-		char[] s2 = new char[s.Length];\
-		string a = \"AGCT\";\
-		for(int i=0; i<s.Length; i++)\
-			s2[i] = ( rg.NextDouble() < p ) ? a[rg.Next()%4] : s[i];\
-		return new String(s2);\
-	}\
-");
+var cs = New.CsObject(`
+	Random rg = new Random();
+	public string RandomizeSeq(string s, double p) {
+		char[] s2 = new char[s.Length];
+		string a = \"AGCT\";
+		for(int i=0; i<s.Length; i++)
+			s2[i] = ( rg.NextDouble() < p ) ? a[rg.Next()%4] : s[i];
+		return new String(s2);
+	}
 
-ExtractVector(200, 20, 750);
-//ExtractSeq(17000);
-//RandomizeSeq(0.7);
-//ExtractSeq2(100);
-//ExtractSeq3(17000);
-//ExtractSeqBin(17000);
-//ExtractSeqBin2(5882);
-//ExtractRandomBin(20);
+	public IList<double> GetFreqVector(dynamic sm, int len, int winSize, int stepSize) {
+		IList<double> ret = New.NumberArray();
+		IList<int> freq = New.IntArray(0,0,0,0);
+		IList<int> win = New.IntArray(); 
+		for(int i=0; i<winSize; i++) 
+			win.Add(0);
+		
+		for(int i=0; i<winSize; i++) {
+		    int idx = sm.GetLetter();
+		    if ( (idx>=0) && (idx<4) ) freq[idx]++;
+		    win[i] = idx;    
+		}
+		int wIdx = 0;
+		for(int i=winSize; i<len; i++) {
+		    if ( (i-winSize) % stepSize == 0 ) {
+		       double a = ((double)freq[0])/winSize;
+			double c = ((double)freq[1])/winSize;
+			double g = ((double)freq[2])/winSize;
+			double t = ((double)freq[3])/winSize;
+			ret.Add(c);
+			ret.Add(g);
+			ret.Add(a);
+			ret.Add(t);
+		    }
+		
+		    int idx = sm.GetLetter();
+		    if ( (idx>=0) && (idx<4) ) freq[idx]++;
+		
+		    int idx2 = win[wIdx];
+		    if ( (idx2>=0) && (idx2<4) )  freq[idx2]--;
+		    win[wIdx] = idx;
+		    wIdx++; if ( wIdx == winSize ) wIdx = 0;
+		}
+		return ret;
+	}
+
+	public void CalculateFreq(dynamic ds, dynamic sm, int winSize, int stepSize, int columnNr	) {
+		int c0 = ds.Columns - columnNr;
+		int rows = ds.Rows;
+
+		for(int row=0; row<rows; row++) {
+			uint idx = (uint)ds.GetValueAt(row, 0);
+			int len = (int)ds.GetValueAt(row, 1);
+			sm.Seek(idx);
+			IList<double> freq = GetFreqVector(sm, len, winSize, stepSize);
+
+		  	for(int col=0; col<columnNr; col++)
+		   		ds.SetDataAt(row, c0+col, ( col < freq.Count ) ? freq[col] : 0);    
+		       if (row>0) {
+		       	if ( row % 100 == 0 ) vv.Echo2(row + ", ");
+		       	if ( row % 1000 == 0 ) vv.Echo("");
+		    	}
+	  	}
+
+	}
+`);
+
 
 //================================================================================
 
@@ -35,43 +83,6 @@ function RandomizeSeq( p ) {
 	vv.Echo("Finished!");
 }
 
-function GetFreqVector(sm, idx, len, winSize, stepSize) {
-  var ret = New.NumberArray();
-  var freq = New.IntArray(0,0,0,0);
-  var win = New.IntArray(); for(var i=0; i<winSize; i++) win.Add(0);
-
-  sm.Seek(idx);
-  for(var i=0; i<winSize; i++) {
-    var idx = sm.GetLetter();
-    if ( (idx>=0) && (idx<4) ) freq[idx]++;
-    win[i] = idx;    
-  }
-  var wIdx = 0;
-  for(var i=winSize; i<len; i++) {
-    if ( (i-winSize) % stepSize == 0 ) {
-       var a = freq[0]/winSize;
-	var c = freq[1]/winSize;
-	var g = freq[2]/winSize;
-	var t = freq[3]/winSize;
-	ret.Add(c);
-	ret.Add(g);
-	ret.Add(a);
-	ret.Add(t);
-	// ret.Add(g);
-	// ret.Add(0.1*(a+t));
-    }
-
-    var idx = sm.GetLetter();
-    if ( (idx>=0) && (idx<4) ) freq[idx]++;
-
-    var idx2 = win[wIdx];
-    if ( (idx2>=0) && (idx2<4) )  freq[idx2]--;
-    win[wIdx] = idx;
-    wIdx++; if ( wIdx == winSize ) wIdx = 0;
-  }
-  return ret;
-}
-
 function ExtractVector(winSize, stepSize, columnNr) {
   var cIdx = ds.IndexOfColumn("C0");
   if ( cIdx >= 0 ) {
@@ -80,20 +91,9 @@ function ExtractVector(winSize, stepSize, columnNr) {
      ds.RemoveColumns(cIdList);
   }
   ds.AddColumns(true, ds.Columns, columnNr);
-  var c0 = ds.Columns - columnNr;
 
-  for(var row=0; row<ds.Rows; row++) {
-    var idx = ds.GetValueAt(row, 0);
-    var len = ds.GetValueAt(row, 1);
-    var freq = GetFreqVector(sm, idx, len, winSize, stepSize);
-    for(var col=0; col<columnNr; col++)
-      ds.SetDataAt(row, c0+col, ( col < freq.Count ) ? freq[col] : 0);
-    if (row>0) {
-        if ( row % 100 == 0 ) vv.Echo2(row + ", ");
-        if ( row % 1000 == 0 ) vv.Echo("");
-    }
-  }
-
+  cs.CalculateFreq(ds, sm, winSize, stepSize, columnNr);
+  ds.CommitChanges();
   vv.Echo("Finished!");
 }
 
@@ -236,4 +236,14 @@ function ExtractRandomBin(maxLen) {
 	ds.CommitChanges();
 	vv.Echo("Finished!");
 }
+
+ExtractVector(200, 20, 750);
+
+//ExtractSeq(17000);
+//RandomizeSeq(0.7);
+//ExtractSeq2(100);
+//ExtractSeq3(17000);
+//ExtractSeqBin(17000);
+//ExtractSeqBin2(5882);
+//ExtractRandomBin(20);
 
