2afc0f813bdcf88257ee7d847b01a8c9119962c9
James Li <jamesxli@visumap.com>
2021-10-03 13:28:09 -0600

    Simplification with GaHelp.js

--- GeneticAnalysis/Package.bat
+++ GeneticAnalysis/Package.bat
@@ -1,7 +1,7 @@
 setlocal
 del *.zip
 copy bin\Release\GeneticAnalysis.dll .
-set JsList=NcbiCdsPost.js EnsemblShowCDS.js SequenceOp.js ShowSeqMap.js Blast.js BlastDb.js
+set JsList=NcbiCdsPost.js EnsemblShowCDS.js SequenceOp.js GaHelp.js ShowSeqMap.js Blast.js BlastDb.js
 zip GeneticAnalysis.zip GeneticAnalysis.dll ICSharpCode.SharpZipLib.dll Install.js UnInstall.js %JsList%
 rem uploadplugin GeneticAnalysis.zip
 endlocal
