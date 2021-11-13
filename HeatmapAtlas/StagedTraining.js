c3ef62272d61a1e1ae3be3cbc1572c706e18ca7b
James Li <jamesxli@visumap.com>
2021-11-13 10:01:15 -0700

    Simplified somewhat

--- HeatmapAtlas/StagedTraining.js
+++ HeatmapAtlas/StagedTraining.js
@@ -1,31 +1,36 @@
-function StagedTraining(loops1=2000, loops2=1000, pp1=0.15, pp2=0.01, exa = 6) {
+function StagedTraining(loops=2500, pp=0.10, exa=12.0) {
 	var t = New.TsneMap();
-	t.MaxLoops = loops1;
+	t.MaxLoops = loops;
 	t.Is3D = false;
-	t.PerplexityRatio = pp1;
+	t.PerplexityRatio = pp;
 	t.Repeats = 1;
 	t.RefreshFreq = 50;
 	t.ExaggerationSmoothen = true;
 	t.ExaggerationFactor = exa;
 	t.AutoScaling = true;
+	t.AutoNormalizing = false;
+	vv.Title = "PP: " + pp;
 
 	t.Show().Reset().Start();
 	if (t.CurrentLoops != t.MaxLoops ) 
 		vv.Return();
-	t.AutoNormalizing = true;
-	t.AutoScaling = true;
-	//New.MapSnapshot().Show();
 
-	t.MaxLoops = loops2;
-	t.ExaggerationFactor = 2.5;
-	t.PerplexityRatio = pp2;
-	t.AutoNormalizing = false;
-	t.Restart().Close();
+	t.ExaggerationFactor = 1.0;
+
+	for(var n=0; n<3; n++) {
+		t.MaxLoops = parseInt(0.5*t.MaxLoops);
+		t.PerplexityRatio = 0.5*t.PerplexityRatio;
+		vv.Title = "PP: " + t.PerplexityRatio;
+		t.Restart();
+		if (t.CurrentLoops != t.MaxLoops ) 
+			vv.Return();
+	}
 	vv.Map.PcaNormalize();
-	New.MapSnapshot().Show();
+	t.Close();
 }
 
-for(var n=0; n<5; n++)
-StagedTraining(2000, 1000, 0.15, 0.01, 6.0);
-
+for(var n=0; n<3; n++) {
+	StagedTraining();
+	New.MapSnapshot().Show();
+}
 
