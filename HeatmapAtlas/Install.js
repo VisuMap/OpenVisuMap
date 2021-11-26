14cd696480bb4d51a88656f4e5f6d8855a23f9e1
James Li <jamesxli@visumap.com>
2021-11-25 18:09:10 -0700

    Set menu/button icon.

--- HeatmapAtlas/Install.js
+++ HeatmapAtlas/Install.js
@@ -1,21 +1,25 @@
 // Install Single-Cell-Atlas related menus mark and run the following statements
 //
-for(var [label, script, view] of [ 
-	["Dual Sorting", "DualSorting.js", "HeatMap"],
-	["Dual Embedding", "DualEmbedding.js", "HeatMap"],
-	["Dual Clustering", "DualClustering.js", "HeatMap"],
-	["Active Genes", "ShowActiveGenes.js", "HeatMap"],
-	["Active Cells", "ShowActiveCells.js", "HeatMap"],	
-	["Save Data", "SaveDataset.js", "HeatMap"],
-	["Compare Maps", "MapMorph.js", "MapSnapshot"],
+var mgr = vv.GuiManager;
+var imgPath = vv.CurrentScriptDirectory + "\\MenuIcon.png";
+var scrPath = "!if (vv.AtlasManager.OpenMap('FeatureMaps', vv.Dataset.Name) == null)vv.Message('No Feature Map Found');";
+
+for(var [label, script, view, img] of [ 
+	["Dual Sorting", "DualSorting.js", "HeatMap", imgPath],
+	["Dual Embedding", "DualEmbedding.js", "HeatMap", null],
+	["Dual Clustering", "DualClustering.js", "HeatMap", null],
+	["Active Genes", "ShowActiveGenes.js", "HeatMap", null],
+	["Active Cells", "ShowActiveCells.js", "HeatMap", null],
+	["Save Data", "SaveDataset.js", "HeatMap", null],
+	["Compare Maps", "MapMorph.js", "MapSnapshot", imgPath],
 ])
-	vv.GuiManager.SetCustomMenu("Atlas/" + label, 
-		true, vv.CurrentScriptDirectory + "/" + script, view);
+	mgr.SetCustomMenu("Atlas/" + label, true, script, view, img);
 
 for(var [label, img, script] of [
-	["Import Loom", vv.CurrentScriptDirectory + "\\MenuIcon.png", "LoomRead.pyn"],
+	["Import Loom", imgPath, "LoomRead.pyn"],
 	["Import H5AD", null, "H5adRead.pyn"],
 	["Import H5", null, "H5Read.pyn"],
-	["Import Matrix", null, "MatrixRead.pyn"] 
+	["Import Matrix", null, "MatrixRead.pyn"], 
+       ["Show Feature Map", null, scrPath],
 ]) 
-	vv.GuiManager.SetCustomButton("Atlas/" + label, img, script);
+	mgr.SetCustomButton("Atlas/" + label, img, script);
--- HeatmapAtlas/Package.bat
+++ HeatmapAtlas/Package.bat
@@ -1,5 +1,5 @@
 set ZipFile=HeatmapAtlas.zip
 del %ZipFile%
-zip %ZipFile%  *.js *.pyn
+zip %ZipFile%  *.js *.pyn MenuIcon.png
 
 copy %ZipFile% ..\..\VisuMapWeb\images
