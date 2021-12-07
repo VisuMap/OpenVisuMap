263f2811068960969cfa01949918f698719f9088
James Li <jamesxli@visumap.com>
2021-12-07 16:30:11 -0700

    Minor extension

--- WaveTransforms/Package.bat
+++ WaveTransforms/Package.bat
@@ -2,3 +2,4 @@ del *.zip
 zip WaveTransforms.zip Readme.txt WaveTransforms.dll *.js 
 zip WaveTransformsSrc.zip Readme.txt *.js  Package.bat *.resx *.cs Properties/AssemblyInfo.cs *.csproj WaveTransforms.rtf
 rem call uploadplugin WaveTransforms.zip
+copy WaveTransforms.zip ..\..\VisuMapWeb\images\WaveTransforms.zip
