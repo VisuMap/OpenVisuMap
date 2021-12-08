a60058c08333f0f7fd63845f566650ea2619b54f
James Li <jamesxli@visumap.com>
2021-12-07 18:11:23 -0700

    Adjustement for the new JS

--- WaveTransforms/Install.js
+++ WaveTransforms/Install.js
@@ -5,12 +5,11 @@
 // Usage: Start VisuMap, then press F5 and select this file to execute. 
 //
 //
+var dir = vv.CurrentScriptDirectory;
+var mgr = vv.GuiManager;
+vv.InstallPlugin("Wave Transforms", dir+"\\WaveTransforms.dll");
 
-var dir = vv.CurrentScriptPath.substr(0, vv.CurrentScriptPath.lastIndexOf("\\") + 1);
-
-vv.InstallPlugin("Wave Transforms", dir +"WaveTransforms.dll");
-
-for(var t in New.StringArray("Fourier", "Haar", "Walsh", "WaveletD4", "PCA") ) {
-  vv.GuiManager.SetCustomMenu("Band-passing by/" + t, true, dir + "BandPass.js", "ValueDiagram");
-  vv.GuiManager.SetCustomMenu("Filtering by/" + t, true, dir + "WaveFilter.js", "<All>");
+for(var t of ["Fourier", "Haar", "Walsh", "WaveletD4", "PCA"] ) {
+  mgr.SetCustomMenu("Band-passing by/" + t, true, "BandPass.js", "ValueDiagram");
+  mgr.SetCustomMenu("Filtering by/" + t, true, "WaveFilter.js", "<All>");
 }	  
