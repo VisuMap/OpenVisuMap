d311f1e441c5a26d20b56db13c760d6a85255045
James Li <jamesxli@visumap.com>
2021-12-07 18:17:51 -0700

    Adjusted

--- DataCleansing/Package.bat
+++ DataCleansing/Package.bat
@@ -3,3 +3,4 @@ copy bin\Release\DataCleansing.dll .
 zip DataCleansing.zip DataCleansing.dll *.js
 del DataCleansing.dll
 rem uploadplugin DataCleansing.zip
+copy DataCleansing.zip ..\..\VisuMapWeb\images\DataCleansing.zip
