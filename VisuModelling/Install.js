b71103cd13bf1989397073aca454e2d2be9403ad
James Li <jamesxli@visumap.com>
2021-12-31 15:22:06 -0700

    Switched to Tensorflow library

--- VisuModelling/Install.js
+++ VisuModelling/Install.js
@@ -2,6 +2,11 @@
 //
 var mgr = vv.GuiManager;
 var icon = vv.CurrentScriptDirectory + "\\Modelling.png";
+
+/*
 mgr.SetCustomButton("VisuModelling/Train", icon, "TrainModel.pyn");
 mgr.SetCustomButton("VisuModelling/*", null, "ModelOps.pyn");
+*/
 
+mgr.SetCustomButton("VisuModelling/Train", icon, "VsTrain.pyn");
+mgr.SetCustomButton("VisuModelling/*", null, "VsOperations.pyn");
