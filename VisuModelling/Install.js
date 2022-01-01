8d22b00cc010da1e862e5620d82341bc8f9af22a
James Li <jamesxli@visumap.com>
2022-01-01 14:25:22 -0700

    Updated

--- VisuModelling/Install.js
+++ VisuModelling/Install.js
@@ -17,7 +17,5 @@ For installing pytorch:
 
 py -3.7 -m pip install torch==1.10.0+cu102 torchvision==0.11.1+cu102 torchaudio===0.10.0+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
 
-For validating GPU for torch in python:
-torch.cuda.is_available()
 */
 
