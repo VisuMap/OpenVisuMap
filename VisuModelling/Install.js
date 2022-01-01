39f741e7aad5140f18e91aefd321fe9f54ad4400
James Li <jamesxli@visumap.com>
2022-01-01 10:45:57 -0700

    Updated

--- VisuModelling/Install.js
+++ VisuModelling/Install.js
@@ -2,11 +2,22 @@
 //
 var mgr = vv.GuiManager;
 var icon = vv.CurrentScriptDirectory + "\\Modelling.png";
-
-/*
-mgr.SetCustomButton("VisuModelling/Train", icon, "PtTrain.pyn");
-mgr.SetCustomButton("VisuModelling/*", null, "PtOperations.pyn");
-*/
-
 mgr.SetCustomButton("VisuModelling/Train", icon, "VsTrain.pyn");
 mgr.SetCustomButton("VisuModelling/*", null, "VsOperations.pyn");
+
+/*
+For using Pytorch library:
+
+var mgr = vv.GuiManager;
+var icon = vv.CurrentScriptDirectory + "\\Modelling.png";
+mgr.SetCustomButton("VisuModelling/Train", icon, "PtTrain.pyn");
+mgr.SetCustomButton("VisuModelling/*", null, "PtOperations.pyn");
+
+For installing pytorch:
+
+py -3.7 -m pip install torch==1.10.0+cu102 torchvision==0.11.1+cu102 torchaudio===0.10.0+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
+
+For validating GPU for torch in python:
+torch.cuda.is_available()
+*/
+
