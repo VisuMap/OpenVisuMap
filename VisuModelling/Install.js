0c4ac0241d14a5915d05ebbc019912cd649e0d84
James Li <jamesxli@visumap.com>
2022-01-01 09:15:08 -0700

    Renamed files

--- VisuModelling/Install.js
+++ VisuModelling/Install.js
@@ -4,8 +4,8 @@ var mgr = vv.GuiManager;
 var icon = vv.CurrentScriptDirectory + "\\Modelling.png";
 
 /*
-mgr.SetCustomButton("VisuModelling/Train", icon, "TrainModel.pyn");
-mgr.SetCustomButton("VisuModelling/*", null, "ModelOps.pyn");
+mgr.SetCustomButton("VisuModelling/Train", icon, "PtTrain.pyn");
+mgr.SetCustomButton("VisuModelling/*", null, "PtOperations.pyn");
 */
 
 mgr.SetCustomButton("VisuModelling/Train", icon, "VsTrain.pyn");
