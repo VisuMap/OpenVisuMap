2ddc46a76bfe578b3d26de3d9e395f17003c4c6c
James Li <jamesxli@visumap.com>
2022-01-04 11:54:53 -0700

    Updated

--- DataLink/Package.bat
+++ DataLink/Package.bat
@@ -2,4 +2,4 @@ del *.zip
 copy bin\Release\DataLink.dll .
 zip DataLink.zip DataLink.dll *.py *.js SP500.csv
 del DataLink.dll
-copy DataModeling.zip ..\..\VisuMapWeb\images
+copy DataLink.zip ..\..\VisuMapWeb\images
