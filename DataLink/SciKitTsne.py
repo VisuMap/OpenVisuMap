fa2f0b62710e21761b063a0a79a3b0148ac2b89f
James Li <jamesxli@visumap.com>
2023-01-07 21:32:19 -0700

    Simplified

--- DataLink/SciKitTsne.py
+++ DataLink/SciKitTsne.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.manifold import TSNE
 
 print("Running sklearn.manifold.TSNE...")
-repeats, mapDim, pPerplexity, epochs = 1, 2, 100, 5000
+mapDim, pPerplexity, epochs = 2, 100, 500
 
 log = DataLinkCmd.DataLinkCmd()
 print('Loading data from VisuMap...')
@@ -17,17 +17,19 @@ if (ds.shape[0]==0) or (ds.shape[1]==0):
         time.sleep(4.0)
         quit()
 print("Loaded table: ", ds.shape)
+log.Close()
 
 print('Fitting data...')
-for rp in range(repeats):
+for rp in range(2):
     t0 = time.time()
     tsne = TSNE(n_components=mapDim, perplexity=pPerplexity, 
         learning_rate=200.0, n_iter=epochs, angle=0.5, n_jobs=-1, verbose=2, init='random')
     map = tsne.fit_transform(ds)
     tm = time.time() - t0
     title = 'SciKit-TSNE: Dimension %d, Perplexity=%.1f, Time: %.1f'%(mapDim, pPerplexity, tm)
-    if mapDim >= 3:
-        log.ShowMatrix(map, view=2, title=title)
-    else:
-        log.ShowMatrix(map, view=12, title=title)
-        log.RunScript('pp.NormalizeView()')
+    with DataLinkCmd.DataLinkCmd() as cmd:
+        if mapDim >= 3:
+            cmd.ShowMatrix(map, view=2, title=title)
+        else:
+            cmd.ShowMatrix(map, view=12, title=title)
+            cmd.RunScript('pp.NormalizeView()')
