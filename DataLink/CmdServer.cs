9073964d25eb57045b90d4371446d7f89048e847
James Li <jamesxli@visumap.com>
2023-08-30 14:10:01 -0600

    Minor simplification: checking for filter name.

--- DataLink/CmdServer.cs
+++ DataLink/CmdServer.cs
@@ -323,8 +323,8 @@ namespace VisuMap.DataLink {
                             IForm frm = DataLink.App.ScriptApp.ToForm(parentForm);
                             if (frm is IExportNumberTable)
                                 dsTable = (frm as IExportNumberTable).GetSelectedNumberTable();
-                            if ( (frm is IVisuMap) && (dsTable != null) && (Root.Data.Map?.FilterName != null) )
-                                dsTable = dsTable.ApplyFilter(Root.Data.Map?.FilterName);
+                            if ((dsTable != null) && ((frm as IVisuMap)?.Map?.Filter != null) )                               
+                                dsTable = dsTable.ApplyFilter((frm as IVisuMap)?.Map?.Filter);
                         } else if (dsName == "$") {  // return the X,Y,Z data point coordinates as a table.
                             IList<IBody> bs = app.Map.SelectedBodies;
                             if ((bs == null) || (bs.Count == 0))
